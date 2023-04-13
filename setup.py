ab7522ef8f22872e3a28bdc337bf75fa6a479db5
Omer Ali <79196757+Omer0191@users.noreply.github.com>
2023-04-13 15:55:23 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -15,10 +15,6 @@ setup(
      "dmr_analysis.script.script_high.others"],
   entry_points = {
      "console_scripts": ['dmr_analysis = dmr_analysis.dmr_analysis:main']} ,
-  version=1.0,
-  description= "Python pipeline for Differential Methylation Region (DMR)  analysis",
-  long_description= long_descr,
-  author= "Junbai Wang",
-  author_email= "junbai@gmail.com"
+
   )
  
